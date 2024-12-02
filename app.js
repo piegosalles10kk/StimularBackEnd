1a0bea81cfbf02d81c2204523a607cd852bd764d
Diego Salles <104213637+piegosalles10kk@users.noreply.github.com>
2024-12-02 12:17:29 -0300

    <update-nivel>

--- app.js
+++ app.js
@@ -37,4 +37,4 @@ connectDB();
 // Connect to Swagger
 setupSwagger(app);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port teste ${PORT}`));
